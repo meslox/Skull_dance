c1bdd0eb5ab9820738f81c0342255ae687ea1d12
meslox <sreenandh369@gmail.com>
2025-09-12 21:10:40 +0530

    setup.sh

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@ if (( EUID != 0 ));then
 	echo "Run as root"
 else
 	cp skull.py /usr/local/bin/skull
-	cp SkullMusic.mp3 /usr/local/bin/SkullMusic.mp3
+	cp SkullMusic.mp3 /etc/SkullMusic.mp3
 	chmod +x /usr/local/bin/skull
 	rm setup.sh
 fi
